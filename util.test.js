63c925c9e4a01ec26e353ba148803a9ff42c72f7
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-04-11 15:16:47 -0300

    create integration test to checkAndGenerate function

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('Rafaela', 21);
     expect(text).toBe('Rafaela (21 years old)');
 });
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Rafaela', 22);
+    expect(text).toBe('Rafaela (22 years old)');
+})
